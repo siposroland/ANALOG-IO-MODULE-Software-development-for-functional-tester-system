e813698dca593b6e5b180ed56f4dd4c0e0d49312
siposroland <siposroland97@gmail.com>
2018-12-06 11:19:40 +0100

    Refresh timer period

--- Src/tim.c
+++ Src/tim.c
@@ -99,7 +99,7 @@ void MX_TIM3_Init(void)
   htim3.Instance = TIM3;
   htim3.Init.Prescaler = 0;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 99;
+  htim3.Init.Period = 4;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
   {
